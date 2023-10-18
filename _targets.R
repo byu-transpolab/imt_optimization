e035e6623fd14bf2532c002e14ac5df62cdfa44c
djarvis3 <daniellinfieldjarvis@gmail.com>
2023-10-18 12:03:49 -0600

    Update _targets.R
    Changes made to sourcefiles and tar_targets

--- _targets.R
+++ _targets.R
@@ -22,22 +22,16 @@ tar_option_set(
 
 # Load the R scripts stored in R/ with your custom functions:
 # for (file in list.files("R", full.names = TRUE)) source(file)
-source("R/data_and_models.R")
 source("R/truck_travel_comparison.R")
 source("R/load_network.R")
+source("R/results_by_type.R")
+source("R/impacted_link_table.R")
+source("R/all_links_plots.R")
 
 # source("other_functions.R") # Source other scripts as needed. # nolint
 
 # Replace the target list below with your own:
 list(
-  tar_target(
-    name = car_mlogit,
-    command = make_data()
-  ),
-  tar_target(
-    name = models,
-    command = estimate_models(car_mlogit)
-  ),
   
   # We add the Network and turn it into a tibble with these targets
   tar_target(
@@ -51,7 +45,37 @@ list(
     command = read_network(network_xml)
   ),
   
-  # We add in the 
+  # We then add in the link delay data with the following targets
+  # The link delays data is produced by multiplying the values in two CSV files 
+  # together, the "data/average_delay" files and the "data/link_volumes" files
+  
+  # The "average_delay" files come from a MATSim output for the average link
+  # delay. We are using the delay from the final (100th) iteration in the output.
+  # These output files are in the Box/MATSim_Outputs folder.
+  
+  # The "link_volumes" files are produced by running the "RunVolumeEventHandler" class
+  # This class uses the network_xml object and the Events from the scenario outputs  "Box\MATSim_Analysis\Volume_Analysis\Events"
+  
+  # Both the "average_delay" and "link_volume" folders are within the data folder
+  # To multiply they together we use the "DelayCalculation.ipynb" python script within the data folder.
+  
+  # All that being said, here is the target for the link delays and their table
+  tar_target(
+    name = delay_table,
+    command = generate_link_delays_table("data/link_delays", network_table)
+  ),
+  
+  # Make the all_links plot
+  tar_target(
+    name = all_links_plot,
+    command = make_all_links_plot(delay_table)
+  ),
+  
+  # Combine the impacted links files into a table
+  tar_target(
+    name = impacted_links_table,
+    command = write_impacted_links_table("data/incident_analysis/delay/incident_link_delays") 
+  ),
   
   # We make the truck travel from the Truck
   tar_target(
