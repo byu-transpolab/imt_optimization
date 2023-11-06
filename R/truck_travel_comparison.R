5aaf97706f2fdeb05ef8634efe935a77f3a6ab2d
djarvis3 <daniellinfieldjarvis@gmail.com>
2023-11-06 10:38:02 -0700

    Updated IMT travel plots
    just changes it from "Incidents" to "Number of Incidents"

--- R/truck_travel_comparison.R
+++ R/truck_travel_comparison.R
@@ -7,7 +7,8 @@ write_truck_time_data <- function(truck_csv) {
   
   truck_time <- truck_time %>%
     select(Seed, Scenario, Average_Time_seconds, Incidents) %>%
-    distinct()
+    distinct() %>%
+    rename(`Number of Incidents` = Incidents)
   
   truck_time$Average_Time_minutes <- truck_time$Average_Time_seconds / 60
   
@@ -18,7 +19,6 @@ write_truck_time_data <- function(truck_csv) {
   
   truck_time$Seed <- factor(truck_time$Seed, levels = ordered_seeds)
   
-  # Mutate the scenario labeling
   truck_time <- truck_time %>%
     mutate(Scenario = case_when(
       Scenario == "Current" ~ "20 IMT",
@@ -29,8 +29,9 @@ write_truck_time_data <- function(truck_csv) {
   return(truck_time)
 }
 
+
 make_truck_time_plot <- function(truck_time) {
-  truck_time_plot <- ggplot(truck_time, aes(x = Seed, y = Average_Time_minutes, size = Incidents)) +
+  truck_time_plot <- ggplot(truck_time, aes(x = Seed, y = Average_Time_minutes, size = `Number of Incidents`)) +
     geom_point(aes(color = as.factor(Scenario), fill = as.factor(Scenario)), 
                position = position_jitter(width = 0.1, height = 0), 
                alpha = 0.7, 
@@ -39,10 +40,11 @@ make_truck_time_plot <- function(truck_time) {
     labs(x = "Seed",
          y = "Time per IMT (mins.)",
          color = "Group",
-         size = "Incidents",
+         size = "Number of Incidents",
          fill = "Group")
   return(truck_time_plot)
 }
+
 
 
 ####################### Distance Plot #####################
@@ -50,24 +52,20 @@ make_truck_time_plot <- function(truck_time) {
 write_truck_distance_data <- function(truck_csv) {
   truck_distance <- read.csv(truck_csv)
   
-  # Reduce to unique combinations of Seed, Scenario, Average Time, and Incidents
   truck_distance <- truck_distance %>%
     select(Seed, Scenario, Average_Distance_meters, Incidents) %>%
-    distinct()
+    distinct() %>%
+    rename(`Number of Incidents` = Incidents)
   
-  # Convert the average distance to miles
   truck_distance$Average_Distance_mile <- truck_distance$Average_Distance_meters / 1609.344
   
-  # Create an ordered vector of Seeds based on Scenario 3 Average Travel Distances
   ordered_seeds <- truck_distance %>%
     filter(Scenario == "Increased") %>%
     arrange(Average_Distance_mile) %>%
     pull(Seed)
   
-  # Reorder Seed factor levels in df_unique based on ordered_seeds
   truck_distance$Seed <- factor(truck_distance$Seed, levels = ordered_seeds)
   
-  # Mutate the scenario labeling
   truck_distance <- truck_distance %>%
     mutate(Scenario = case_when(
       Scenario == "Current" ~ "20 IMT",
@@ -78,9 +76,8 @@ write_truck_distance_data <- function(truck_csv) {
   return(truck_distance)
 }
 
-
 make_truck_distance_plot <- function(truck_distance) {
-  truck_distance_plot <- ggplot(truck_distance, aes(x = Seed, y = Average_Distance_mile, size = Incidents)) +
+  truck_distance_plot <- ggplot(truck_distance, aes(x = Seed, y = Average_Distance_mile, size = `Number of Incidents`)) +  # Adjusted for new column name
     geom_point(aes(color = as.factor(Scenario), fill = as.factor(Scenario)), 
                position = position_jitter(width = 0.1, height = 0), 
                alpha = 0.7, 
@@ -89,7 +86,7 @@ make_truck_distance_plot <- function(truck_distance) {
     labs(x = "Seed",
          y = "Distance per IMT (miles)",
          color = "Group",
-         size = "Incidents",
+         size = "Number of Incidents",
          fill = "Group")
   return(truck_distance_plot)
 }
